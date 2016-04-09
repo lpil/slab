5d73ddc9b0f581ce4e47797d4a9d5bd84701afe2
Louis Pilfold <louis@lpil.uk>
2016-04-09 20:20:26 +0100

    Test classes, ids, and preceeding spaces

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -1,9 +1,11 @@
 defmodule Slab.ParserTest do
-  use ExUnit.Case, async: false # For printing
+  use ExUnit.Case, async: false # TODO: Make Sync. Async for printing.
 
-  defmacro text ~> tree do
+  alias Slab.ParserElement, as: E
+
+  defmacro text ~> expected do
     quote bind_quoted: binding do
-      assert Slab.Parser.parse!(text) == tokens
+      assert Slab.Parser.parse!(text) == expected
     end
   end
 
@@ -14,10 +16,38 @@ defmodule Slab.ParserTest do
   end
 
 
-  test "classes" do
+  test "dev printing" do
     print """
     h3 f
     h3  f
     """
+  end
+
+  test "preceeding spaces" do
+    """
+    h1 o
+    h2  o
+    h3   o
+    """ ~> [
+      %E{labels: [], type: 'h1', text: 'o'  }, :nl,
+      %E{labels: [], type: 'h2', text: ' o' }, :nl,
+      %E{labels: [], type: 'h3', text: '  o'},
+    ]
+  end
+
+  test "classes and ids" do
+    """
+    .hi Yo
+    #there
+    .c#i
+    a.you
+    b#today
+    """ ~> [
+      %E{labels: [class: 'hi'], text: 'Yo' }, :nl,
+      %E{labels: [id: 'there']             }, :nl,
+      %E{labels: [class: 'c', id: 'i']     }, :nl,
+      %E{labels: [class: 'you'], type: 'a' }, :nl,
+      %E{labels: [id: 'today'],  type: 'b' },
+    ]
   end
 end
